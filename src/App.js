231e52ec09c70cbf6eba3586edaaffc8e8b9d2d0
overy1234 <overy1234@naver.com>
2019-11-07 10:36:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>시작합니다.</h2>
+       
+     
+    </div>
+  );
+}
+
+export default App;
